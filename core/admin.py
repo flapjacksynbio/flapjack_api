65a8d8fbb0ac45dfe2c2f8aadec91a16ff79cb96
Guillermo <gyanezfeliu@gmail.com>
2020-05-11 21:27:19 -0400

    models modifiable in the admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# Models modifiable in the admin
+admin.site.register(Study)
+admin.site.register(Assay)
+admin.site.register(Strain)
+admin.site.register(Media)
+admin.site.register(Dna)
+admin.site.register(Sample)
+admin.site.register(Measurement)
+admin.site.register(Inducer)
+admin.site.register(Signal)
