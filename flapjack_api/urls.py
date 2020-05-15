cca73ddc6cfeef7b937672ac827c3677da6664c5
Verner Codoceo <vacodoceo@uc.cl>
2020-05-15 03:07:41 -0400

    Add jwt auth

--- flapjack_api/urls.py
+++ flapjack_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('registry.urls')),
-    path('admin/', admin.site.urls),
+    path('jwtauth/', include('jwtauth.urls'), name='jwtauth'),
 ]
