ddcb408a31bb2193822ae2e7876d47b63abe2d92
Tim Rudge <timrudge@gmail.com>
2020-05-15 11:33:56 -0400

    Add router and register viewsets for registry app

--- registry/urls.py
+++ registry/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
-
 from . import views
+from rest_framework import routers
+from django.conf import settings
+
+router = routers.DefaultRouter()
+router.register(r'study', views.StudyViewSet)
+router.register(r'assay', views.AssayViewSet)
+router.register(r'sample', views.SampleViewSet)
+router.register(r'dna', views.DnaViewSet)
+router.register(r'media', views.MediaViewSet)
+router.register(r'strain', views.StrainViewSet)
+router.register(r'inducer', views.InducerViewSet)
+router.register(r'measurement', views.MeasurementViewSet)
+router.register(r'signal', views.SignalViewSet)
+router.register(r'inducer', views.InducerViewSet)
+router.register(r'user', views.UserViewSet)
+router.register(r'group', views.GroupViewSet)
+router.register(r'user_profile_info', views.UserProfileInfoViewSet)
 
 urlpatterns = [
-  path('', views.index, name='index'),
-]
+  url(r'^api/', include(router.urls))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
