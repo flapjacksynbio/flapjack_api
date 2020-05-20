13bf8cc93945e78e5392998d7bc647498ffa3464
Tim Rudge <timrudge@gmail.com>
2020-05-20 17:26:52 -0400

    Pagination and authentication setup for rest-framework

--- flapjack_api/settings.py
+++ flapjack_api/settings.py
@@ -113,3 +113,13 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
+
+# rest framework config
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+    ]
+}
