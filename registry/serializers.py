28c9c7f403c9e21db5793d8717b9bde1505b0957
Tim Rudge <timrudge@gmail.com>
2020-05-20 17:46:30 -0400

    Switch to ModelSerializer instead of HyperLinkedModelSerializer, now objects represented by ids

--- registry/serializers.py
+++ registry/serializers.py
@@ -2,49 +2,49 @@ from rest_framework import serializers
 from .models import Assay, Dna, Inducer, Measurement, Media, Sample, Signal, Strain, Study
 
 
-class StudySerializer(serializers.HyperlinkedModelSerializer):
+class StudySerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Study
         fields = '__all__'
 
-class AssaySerializer(serializers.HyperlinkedModelSerializer):
+class AssaySerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Assay
         fields = '__all__'
 
-class SampleSerializer(serializers.HyperlinkedModelSerializer):
+class SampleSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Sample
         fields = '__all__'
 
-class DnaSerializer(serializers.HyperlinkedModelSerializer):
+class DnaSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Dna
         fields = '__all__'
 
-class MediaSerializer(serializers.HyperlinkedModelSerializer):
+class MediaSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Media
         fields = '__all__'
 
-class StrainSerializer(serializers.HyperlinkedModelSerializer):
+class StrainSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Strain
         fields = '__all__'
 
-class InducerSerializer(serializers.HyperlinkedModelSerializer):
+class InducerSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Inducer
         fields = '__all__'
 
-class MeasurementSerializer(serializers.HyperlinkedModelSerializer):
+class MeasurementSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Measurement
@@ -76,26 +76,26 @@ class MeasurementSerializer(serializers.HyperlinkedModelSerializer):
     '''
 
 
-class SignalSerializer(serializers.HyperlinkedModelSerializer):
+class SignalSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Signal
         fields = '__all__'
         
 '''
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = User
         fields = '__all__'
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = Group
         fields = '__all__'
 
-class UserProfileInfoSerializer(serializers.HyperlinkedModelSerializer):
+class UserProfileInfoSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     class Meta:
         model = UserProfileInfo
