5734a6c07d2745e6cb0665dca0b0e0ce476c912c
MrEarle <benjaminearle123@gmail.com>
2020-06-19 22:01:31 -0400

    Added handling for plot

--- registry/consumers.py
+++ registry/consumers.py
@@ -14,12 +14,12 @@ class RegistryConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-        await self.channel_layer.group_send(
-            "asd",
-            {
-                'type': 'update_load_status',
-            }
-        )
+        # await self.channel_layer.group_send(
+        #     "asd",
+        #     {
+        #         'type': 'update_load_status',
+        #     }
+        # )
 
     async def update_load_status(self, event):
         # Here helper function fetches live score from DB.
@@ -31,11 +31,31 @@ class RegistryConsumer(AsyncWebsocketConsumer):
 
     # TO DO: setup message receiving and disconnection
 
+    async def generate_data(self, event):
+        parameters = event['params']
+        values = []
+        for i in range(1, 101, 5):
+            values.append((i, i ** 2))
+            await self.send(text_data=json.dumps({
+                'type': 'progress_update',
+                'data': {
+                    'progress': i
+                }
+            }))
+            await asyncio.sleep(0.1)
+        await self.send(text_data=json.dumps({
+            'type': 'plot_data',
+            'data': {
+                'values': values
+            }
+        }))
+
     async def receive(self, text_data):
-        print(text_data)
+        data = json.loads(text_data)
+        if data['type'] == 'plot':
+            await self.generate_data({'params': data['parameters']})
 
     async def websocket_disconnect(self, message):
-        print(message)
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
