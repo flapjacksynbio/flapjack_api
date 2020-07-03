1be3acbd7ea8e06fbd8798ca7c3e4f17a7aca12a
Verner Codoceo <vacodoceo@uc.cl>
2020-07-03 05:19:42 -0400

    Add user recognition to plot websockets

---
+++ flapjack_api/channels_middleware.py
@@ -0,0 +1,54 @@
+# https://hashnode.com/post/using-django-drf-jwt-authentication-with-django-channels-cjzy5ffqs0013rus1yb9huxvl
+
+from django.db import close_old_connections
+from rest_framework_simplejwt.tokens import UntypedToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from jwt import decode as jwt_decode
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from urllib.parse import parse_qs
+
+
+class TokenAuthMiddleware:
+    """
+    Custom token auth middleware
+    """
+
+    def __init__(self, inner):
+        # Store the ASGI application we were passed
+        self.inner = inner
+
+    def __call__(self, scope):
+
+        # Close old database connections to prevent usage of timed out connections
+        close_old_connections()
+
+        # Get the token
+        token = parse_qs(scope["query_string"].decode("utf8"))["token"][0]
+
+        # Try to authenticate the user
+        try:
+            # This will automatically validate the token and raise an error if token is invalid
+            UntypedToken(token)
+        except (InvalidToken, TokenError) as e:
+            # Token is invalid
+            print(e)
+            return None
+        else:
+            #  Then token is valid, decode it
+            decoded_data = jwt_decode(
+                token, settings.SECRET_KEY, algorithms=["HS256"])
+            # print(decoded_data)
+            # Will return a dictionary like -
+            # {
+            #     "token_type": "access",
+            #     "exp": 1568770772,
+            #     "jti": "5c15e80d65b04c20ad34d77b6703251b",
+            #     "user_id": 6
+            # }
+
+            # Get the user using ID
+            user = get_user_model().objects.get(id=decoded_data["user_id"])
+
+        # Return the inner application directly and let it run everything else
+        return self.inner(dict(scope, user=user))
--- flapjack_api/settings.py
+++ flapjack_api/settings.py
@@ -140,3 +140,5 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
