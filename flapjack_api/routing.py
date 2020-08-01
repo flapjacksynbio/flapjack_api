32fb577e7b05862b845da043a21eea3647d65ab8
Tim Rudge <timrudge@gmail.com>
2020-08-01 11:42:21 -0400

    Add route for analysis websocket

--- flapjack_api/routing.py
+++ flapjack_api/routing.py
@@ -1,5 +1,6 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from plot.routing import websockets as plotWs
+from analysis.routing import websockets as analysisWs
 from registry.routing import websockets as uploadWs
 
 from django.conf.urls import url
@@ -7,6 +8,7 @@ from django.conf.urls import url
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
         url('^ws/plot', plotWs),
+        url('^ws/analysis', analysisWs),
         url('^ws/registry', uploadWs)
     ]),
 })
