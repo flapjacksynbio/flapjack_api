f31f259c2c30ffccabb460e0866864573009902e
Tim Rudge <timrudge@gmail.com>
2020-08-01 06:52:11 -0400

    Set up websockets for analysis app

---
+++ analysis/routing.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from flapjack_api.channels_middleware import TokenAuthMiddleware
+from .consumers import AnalysisConsumer
+
+
+websockets = ProtocolTypeRouter({
+    "websocket": TokenAuthMiddleware(
+        URLRouter([
+            path(
+                "analysis", AnalysisConsumer,
+                name="analysis-ws",
+            ),
+        ])
+    ),
+})
