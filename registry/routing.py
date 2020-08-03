0773107b6eb266f66352bdafd413fd601dd94876
Verner Codoceo <vacodoceo@uc.cl>
2020-08-03 00:54:54 -0400

    Add auth to upload ws

--- registry/routing.py
+++ registry/routing.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from flapjack_api.channels_middleware import TokenAuthMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
-
 from .consumers import RegistryConsumer
-websockets = URLRouter([
-    path(
-        "upload", RegistryConsumer,
-        name="registry-ws",
+
+websockets = ProtocolTypeRouter({
+    "websocket": TokenAuthMiddleware(
+        URLRouter([
+            path(
+                "upload", RegistryConsumer,
+                name="registry-ws",
+            ),
+        ])
     ),
-])
+})
