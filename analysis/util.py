10d65f7a7f30ed6cc0da8048c316b7e731ae2238
Tim Rudge <timrudge@gmail.com>
2020-08-05 17:56:25 -0400

    Add required wellfare import

--- analysis/util.py
+++ analysis/util.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import wellfare as wf
 
 # Model functions for fitting to data
 # -----------------------------------------------------------------------------------
