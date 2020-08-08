42571fae05c5a4b5626587db42c71c562521fe96
Tim Rudge <timrudge@gmail.com>
2020-08-07 21:37:50 -0400

    Connecting up analysis consumer

--- analysis/consumers.py
+++ analysis/consumers.py
@@ -4,7 +4,8 @@ import asyncio
 # Third Party imports.
 from channels.exceptions import DenyConnection
 from channels.generic.websocket import AsyncWebsocketConsumer
-from . import analysis
+from analysis.analysis import Analysis 
+from analysis.util import *
 from registry.util import get_samples, get_measurements
 from plotly.subplots import make_subplots
 import plotly
@@ -27,10 +28,36 @@ class AnalysisConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         if data['type'] == 'analysis':
             print(data, flush=True)
-            #await self.generate_data({'params': data['parameters']})
+            await self.generate_data({'params': data['parameters']})
 
     async def disconnect(self, message):
         await self.channel_layer.group_discard(
             "analysis",
             self.channel_name
         )
+
+    async def generate_data(self, params):
+        signals = params.get('signalIds')
+        if analysis:
+            s = get_samples(params)
+            df = get_measurements(s, signals)
+            analysis = Analysis(params, signals)
+            df = self.run_analysis(df, analysis)
+
+    async def run_analysis(self, df, analysis):
+        grouped = df.groupby('Sample')
+        result_dfs = []
+        n_samples = len(grouped)
+        progress = 0
+        for id,g in grouped:
+            result_df = analysis.analyze_data(g)
+            result_dfs.append(result_df)
+            progress += 1
+            await self.send(text_data=json.dumps({
+                'type': 'progress_update',
+                'data': {'progress': int(100 * progress / n_samples)}
+            }))
+            await asyncio.sleep(0)
+        df = pd.concat(result_dfs)
+        return df
+
