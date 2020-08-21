e6ef37e06a17769f279875808900c3e3b1e07c03
Guillermo <gyanezfeliu@gmail.com>
2020-08-21 17:03:38 -0400

    test

--- registry/consumers.py
+++ registry/consumers.py
@@ -502,4 +502,5 @@ class UploadConsumer(AsyncWebsocketConsumer):
             await self.progress_update(process_percent)
 
         Measurement.objects.bulk_create(measurements)
-        
+
+print("FKN TEST")
