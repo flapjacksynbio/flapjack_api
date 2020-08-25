ad5e49714c944e653f12ad50f06c21997736a9f0
Guillermo <gyanezfeliu@gmail.com>
2020-08-25 17:09:15 -0400

    username and email lookup_expr='icontains'

--- registry/permissions.py
+++ registry/permissions.py
@@ -166,3 +166,14 @@ class MeasurementPermission(IsAuthenticated):
             return True
         else:
             return request.user == obj.sample.assay.study.owner
+
+class UserPermission(IsAuthenticated):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in SAFE_METHODS:
+            return True
